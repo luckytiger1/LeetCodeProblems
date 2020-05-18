0fa7356413ba3973bace715c1302d4199051b71a
Daniyar Ketebay <daniyar.abc@gmail.com>
2020-05-18 15:33:18 +0600

    feat: add solution of firstbadversion

---
+++ MayChallenge/FirstBadVersion/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int res = s.firstBadVersion(4);
+        System.out.println(res);
+    }
+}
--- MayChallenge/FirstBadVersion/src/Solution.java
+++ MayChallenge/FirstBadVersion/src/Solution.java
@@ -1,2 +1,20 @@
-public class Solution {
+public class Solution  {
+    public int firstBadVersion(int N) {
+        int l = 0, r = N;
+
+        while(l < r) {
+            int mid = l + (r - l)/2;
+
+            if(isBadVersion(mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+    private boolean isBadVersion(int n) {
+        return true;
+    }
 }
