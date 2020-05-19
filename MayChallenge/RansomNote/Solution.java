b15ed44cbdfb089903626475ae431cde9afb6fe4
Daniyar Ketebay <daniyar.abc@gmail.com>
2020-05-19 22:46:46 +0600

    docs: add descriptions

--- MayChallenge/RansomNote/Solution.java
+++ MayChallenge/RansomNote/Solution.java
@@ -2,9 +2,27 @@ package RansomNote;
 
 import java.util.ArrayList;
 
+/*
+ 383. Ransom Note https://leetcode.com/problems/ransom-note/
+
+ Given an arbitrary ransom note string and another string
+ containing letters from all the magazines, write a function that
+ will return true if the ransom note can be constructed from the magazines ;
+ otherwise, it will return false.
+
+ Each letter in the magazine string can only be used once in your ransom note.
+
+ Example 1:
+ Input: ransomNote = "a", magazine = "b"
+ Output: false
+
+*/
+
 public class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         ArrayList<Character> charArr = new ArrayList<>();
+
+
         for(char c : magazine.toCharArray()) {
             charArr.add(c);
         }
