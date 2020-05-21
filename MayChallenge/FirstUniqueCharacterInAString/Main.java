c138e95c5c70809b0244ba4024fd13770aed8f85
Daniyar Ketebay <daniyar.abc@gmail.com>
2020-05-21 22:19:30 +0600

    feat: add FirstUniqueCharacterInAString solution

---
+++ MayChallenge/FirstUniqueCharacterInAString/Main.java
@@ -0,0 +1,10 @@
+package FirstUniqueCharacterInAString;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String str = "loveleetcode";
+        int res = s.firstUniqChar(str);
+        System.out.println(res);
+    }
+}
